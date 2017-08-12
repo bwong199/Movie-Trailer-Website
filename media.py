27a74acf4ada37127e6f8f08da8269f9bddc580d
bwong199 <bwong199@my.bcit.ca>
2017-08-12 10:13:17 -0600

    First Commit

--- media.py
+++ media.py
@@ -1,10 +1,18 @@
 import json
 import fresh_tomatoes
 
+
+# this is the movie class with a constructor that takes 1)title, 
+# 2) post_image_url, and 3) youtube url trailer as inputs
+
+
 class movie:
-    def __init__(self, title, poster_image_url, trailer_youtube_url):
+    def __init__(
+            self,
+            title,
+            poster_image_url,
+            trailer_youtube_url,
+    ):
         self.title = title
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
-
-
---
+++ readme.md
@@ -0,0 +1,3 @@
+1) In the Movie_Trailer_Website directory, type "python entertainment_center.py"
+2) This will open up the application 
+3) Click on any of the movie posters to watch the Youtube trailer
